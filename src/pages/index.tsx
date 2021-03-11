0f7910163d3650dc6a26c50396154a85ba5d74a1
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-03-11 15:41:16 -0300

    challengesadd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+
 
 export default function Home() {
   return (
@@ -22,7 +24,7 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-
+          <ChallengeBox />
         <div>
 
         </div>
