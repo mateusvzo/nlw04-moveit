224286293de112ddc62acaedee818001dfea4991
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-03-19 13:23:22 -0300

    finish app

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -23,7 +23,7 @@ let countdownTimeOut: NodeJS.Timeout;
 export function CountdownProvider({children}) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(0.1 * 60);
+    const [time, setTime] = useState(30 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
